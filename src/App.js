5483628bf59e73b96c0acbcd60f700de6e3d31b3
sumukhah <sumukha214@gmail.com>
2022-06-14 21:50:48 +0530

    react-phyllo-integration

--- src/App.js
+++ src/App.js
@@ -1,22 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import logo from "./logo.svg";
+
+// configure it in your .env file
+const config = {
+  clientDisplayName: client_display_name_here,
+  environment: environment_here,
+  userId: user_id_here,
+  token: user_token,
+  redirect: redirect_or_popup,
+  workPlatformId: work_platform_id_or_null,
+};
 
 function App() {
+  const [phylloConnectSDK, setPhylloConnectSDK] = useState({});
+
+  useEffect(() => {
+    const PhylloConnect = window.PhylloConnect;
+    const phylloConnect = PhylloConnect.initialize(config);
+    phylloConnect.on(
+      "accountConnected",
+      (accountId, workplatformId, userId) => {
+        console.log(
+          `onAccountConnected: ${accountId}, ${workplatformId}, ${userId}`
+        );
+      }
+    );
+    phylloConnect.on(
+      "accountDisconnected",
+      (accountId, workplatformId, userId) => {
+        console.log(
+          `onAccountDisconnected: ${accountId}, ${workplatformId}, ${userId}`
+        );
+      }
+    );
+    phylloConnect.on("tokenExpired", (userId) => {
+      console.log(`onTokenExpired: ${userId}`);
+    });
+    phylloConnect.on("exit", (reason, userId) => {
+      console.log(`onExit: ${reason}, ${userId}`);
+    });
+    phylloConnect.on("connectionFailure", (reason, workplatformId, userId) => {
+      console.log(
+        `onConnectionFailure: ${reason}, ${workplatformId}, ${userId}`
+      );
+    });
+    setPhylloConnectSDK(phylloConnect);
+  }, []);
+
+  const redirectToPhylloSDK = () => {
+    phylloConnectSDK.open();
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={redirectToPhylloSDK}>
+          Click me to go to phyllo sdk
+        </button>
       </header>
     </div>
   );
